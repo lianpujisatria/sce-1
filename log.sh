769b4db0ace5548310fde6b3f3570d18b764a03e
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-07-06 11:58:29 +0700

    F

--- log.sh
+++ log.sh
@@ -7,12 +7,12 @@ echo "" | tee -a log-install.txt
 echo "   >>> Service Port" | tee-a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 443, 445, 990"  | tee -a log-install.txt
-echo "   - Dropbear                : 109, 143, 110"  | tee -a log-install.txt
+echo "   - Stunnel4                : 443, 171"  | tee -a log-install.txt
+echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
 echo "   - WebSocket               : 2086"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP SSH)"  | tee -a log-install.txt
-echo "   - Badvpn                  : 7000, 7300, 7700"  | tee -a log-install.txt
-echo "   - Nginx                   : 85"  | tee -a log-install.txt
+echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
+echo "   - Nginx                   : 81"  | tee -a log-install.txt
 echo "   - Wireguard               : 7070"  | tee -a log-install.txt
 echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
 echo "   - PPTP VPN                : 1732"  | tee -a log-install.txt
@@ -44,4 +44,4 @@ echo ""  | tee -a log-install.txt
 echo "=================================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
 echo "- Mod By M AFDHAN & NezaVPN" | tee -a log-install.txt
 echo ""
-rm -f /root/log.sh
+echo "Done Add Log"
