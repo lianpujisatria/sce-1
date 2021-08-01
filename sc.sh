dd592a01bc84223121e8eb8a07e0b14e9dcf2aa6
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-01 23:29:01 +0700

    F

--- sc.sh
+++ sc.sh
@@ -75,9 +75,11 @@ echo "" | tee -a log-install.txt
 echo "   >>> Service Port" | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 443, 171"  | tee -a log-install.txt
+echo "   - Stunnel4                : 567, 171"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
-echo "   - WebSocket               : 2082"  | tee -a log-install.txt
+echo "   - WebSocket Dropbear      : 2095"  | tee -a log-install.txt
+echo "   - WebSocket OpenVPN       : 2086"  | tee -a log-install.txt
+echo "   - WebSocket TLS           : 445"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP SSH)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
@@ -88,7 +90,7 @@ echo "   - SSTP VPN                : 444"  | tee -a log-install.txt
 echo "   - Shadowsocks-R           : 1443-1543"  | tee -a log-install.txt
 echo "   - SS-OBFS TLS             : 2443-2543"  | tee -a log-install.txt
 echo "   - SS-OBFS HTTP            : 3443-3543"  | tee -a log-install.txt
-echo "   - V2RAY Vmess TLS         : 8443"  | tee -a log-install.txt
+echo "   - V2RAY Vmess TLS         : 443"  | tee -a log-install.txt
 echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
