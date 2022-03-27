e8384b48356a4b3f91a4bdbcb7001c3e01f2b467
Afdhan <77567725+Afdhan@users.noreply.github.com>
2022-03-27 19:24:55 +0700

    Update w.sh

--- w.sh
+++ w.sh
@@ -44,6 +44,19 @@ rm -f websock.sh
 
 rm -f ins.sh
 
+
+cat > /usr/bin/bersih << END
+#!/bin/bash
+echo 1 > /proc/sys/vm/drop_caches
+echo 2 > /proc/sys/vm/drop_caches
+echo 3 > /proc/sys/vm/drop_caches
+swapoff -a
+swapon -a
+ban
+clear-log
+END
+chmod +x /usr/bin/bersih
+
 cat <<EOF> /etc/systemd/system/autosett.service
 
 [Unit]
